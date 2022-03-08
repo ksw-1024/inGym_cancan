aab0f72395d2f03a3fdcd574003af1367d698f8e
ksw-1024 <88093046+ksw-1024@users.noreply.github.com>
2022-03-08 21:04:19 +0900

    Add files via upload

--- javascripts/link.js
+++ javascripts/link.js
@@ -1,23 +1,23 @@
 function index() {
-    location.href = "./index.html?20220308-004"
+    location.href = "./index.html?20220308-005"
 }
 
 function about() {
-    location.href = "./about.html?20220308-003"
+    location.href = "./about.html?20220308-004"
 }
 
 function music() {
-    location.href = "./music.html?20220308-003"
+    location.href = "./music.html?20220308-004"
 }
 
 function schedule() {
-    location.href = "./schedule.html?20220308-002"
+    location.href = "./schedule.html?20220308-003"
 }
 
 function voluntary() {
-    location.href = "./voluntary.html?20220308-002"
+    location.href = "./voluntary.html?20220308-003"
 }
 
 function credit() {
-    location.href = "./credit.html?20220308-002"
+    location.href = "./credit.html?20220308-003"
 }
